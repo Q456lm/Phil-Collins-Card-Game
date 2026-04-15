394e759b6da3ed74d0532c8743f5ef0d11f8b093
Clarence Bunting <cbbunting1@my.waketech.edu>
2026-04-15 10:06:29 -0400

    It won't run waaaaahhhh

---
+++ card.java
@@ -0,0 +1,7 @@
+public class card{
+    private int cardType;
+
+    public card(int cardNum){
+        cardType = cardNum;//Determines the type of card.
+    }
+}
--- game.java
+++ game.java
@@ -1,1 +1,24 @@
+public class game{
 
+    public static ArrayList<int>() cardNums = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15));
+
+
+    public static int intInput(String msg){ //Gets Integer User Input
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(msg);
+            int ret = scanner.nextInt();
+            //scanner.close();
+            return ret;
+        }except(Exception e){
+            System.out.println("Starting game with 1 player");
+            return 1;
+        }
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+}
