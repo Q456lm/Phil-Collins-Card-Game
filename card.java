1a7ec03c4733274f01f6ec609afa7346f6943986
Clarence Bunting <cbbunting1@my.waketech.edu>
2026-04-16 10:16:47 -0400

    *Guitar Solo*

--- card.java
+++ card.java
@@ -18,6 +18,26 @@ public class card{
             return "One More Night - If you have 2 cards or less (after using this card), draw 6 cards of any type.";
         }else if (cardType == 6){
             return "Easy Lover - Target One Player to Deposit Three Cards";
+        }else if (cardType == 7){
+            return "Against All Odds - Stop the effects against you for all other cards on this turn.";
+        }else if (cardType == 8){
+            return "Sussudio - Draw 3 cards of any type";
+        }else if (cardType == 9){
+            return "Don’t Lose My Number - However many cards are in your hand when you use this card if how many cards must be in your hand after 2 turns.";
+        }else if (cardType == 10){
+            return "I Wish It Would Rain Down - Pick a Player and Steal one of their album cards";
+        }else if (cardType == 11){
+            return "Do You Remember? - Look at another player's cards then next turn steal one of their song cards.";
+        }else if (cardType == 12){
+            return "Two Hearts - If Players > 2: Pick 2 Players and Steal 1 song Card from each of them. If only 2 players: Steal 2 cards from the other player";
+        }else if (cardType == 13){
+            return "You Can’t Hurry Love - You can use 2 cards this turn after this.";
+        }else if (cardType == 14){
+            return "That’s All - Deposit 2 song cards and draw 2 new song cards and start a divorce";
+        }else if (cardType == 15){
+            return "I Can’t Dance - Pick another player to deposit 3 song cards";
+        }else{
+            return "Nothing - It Signify That There is a Logic Error in my code";
         }
     }
 }
--- game.java
+++ game.java
@@ -1,24 +1,70 @@
+import java.util.*;
+
 public class game{
 
-    public static ArrayList<int>() cardNums = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15));
+    public static ArrayList<Integer> cardNums = new ArrayList<Integer>(Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15));
+    public static ArrayList<ArrayList<card>> allHands = new ArrayList<ArrayList<card>>();
+    public static ArrayList<card> discardPile = new ArrayList<>();
 
+    public static int random(int num1,int num2){ //Generates a random number between two values
+        int num3=(num2-num1)+1;
+        int ran_num = (int) (Math.random()*num3)+num1;
+        return ran_num;
+    }
 
     public static int intInput(String msg){ //Gets Integer User Input
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.print(msg);
             int ret = scanner.nextInt();
-            //scanner.close();
             return ret;
-        }except(Exception e){
-            System.out.println("Starting game with 1 player");
-            return 1;
+        }catch(Exception e){
+            return 2; 
         }
+    }
+
+    public static void startCards(int playerNums){
+        for (int i = 0; i < playerNums; i++){
+            allHands.add(new ArrayList<card>());
+        }
+
+
+        for (int i = 0; i < allHands.size(); i++){
+            for (int j = 0; j < 7; j++){
+                int randomNum = random(0, cardNums.size()-1);
+                allHands.get(i).add(new card(cardNums.get(randomNum)));
+                cardNums.remove(randomNum);
+            }
+        }
+    }
+
+    public static void displayCards(int curPlayer){
+        for (int i = 0; i < allHands.get(curPlayer-1).size(); i++){
+            System.out.println(allHands.get(curPlayer-1).get(i));
+        }
+    }
+
+    public static void viewRules(){
+        System.out.println("First player to get empathy to zero wins. Your empathy represents the total amount of cards you can have in your hand. Each player starts with 8 cards and 21 empathy. You lose empathy by releasing an album, each album has different conditions to be released. Once you release all your albums you run out of empathy.");
     }
 
 
 
     public static void main(String[] args){
+        System.out.println("Welcome to Phil Collins Card Game");
+        boolean menu = true;
+        while (menu){
+            int check = intInput("What do you want to do. \n 1. Start Game \n 2. View Rules \n: ");
+            if (check == 1){
+                menu = false;
+            }else if (check == 2){
+               viewRules();
+            }
+        }
+        int playerNums = intInput("How many players? ");
+        startCards(playerNums);
 
     }
+
+
 }
